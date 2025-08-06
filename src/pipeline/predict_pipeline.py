f767da20438992bcd375e84776168543e5e4224f
IaManav <140186610+IaManav@users.noreply.github.com>
2025-08-06 14:41:18 +0530

    prediction_pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,58 @@
+import sys
+import pandas as pd
+
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path='artifacts/model.pkl'
+            preprocessor_path='artifacts/preprocessor.pkl'
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("after loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+
+            return preds
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+class Custom_Data:
+    def __init__(self,year:int,selling_price:int,km_driven:int,fuel:str,seller_type:str,transmission:str,owner:str,mielage:str,engine:int,max_power:int,seat:int):
+        self.year=year
+        self.selling_price=selling_price
+        self.km_driven=km_driven
+        self.fuel=fuel
+        self.seller_type=seller_type
+        self.transmission=transmission
+        self.owner=owner
+        self.mielage=mielage
+        self.engine=engine
+        self.max_power=max_power
+        self.seat=seat
+
+    def get_data_as_dataframe(self):
+        try:
+            Custom_Data_input_dict={
+                "year":[self.year],
+                "selling_price":[self.selling_price],
+                "km_driven":[self.km_driven],
+                "fuel":[self.fuel],
+                "seller_type":[self.seller_type],
+                "transmission":[self.transmission],
+                "owner":[self.owner],
+                "mielage":[self.mielage],
+                "engine":[self.engine],
+                "max_power":[self.max_power],
+                "seat":[self.seat],
+            }
+
+            pd.DataFrame(Custom_Data_input_dict)
+        except Exception as e:
+            raise CustomException(e,sys)
